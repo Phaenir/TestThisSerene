34a1941d54cc4cd59ea3e0ffe52e7bf3e6d81778
Ruslan <phaenir@gmail.com>
2016-11-20 18:56:01 +0100

    Created Database Movie
    p. 45

---
+++ TestThisSerene/TestThisSerene.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20161120_180900_MovieTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace TestThisSerene.Migrations.DefaultDB
+{
+    [Migration(20161120180900)]
+    public class DefaultDB_20161120_180900_MovieTable:Migration
+    {
+        public override void Up()
+        {
+            Create.Schema("mov");
+            Create.Table("Movie").InSchema("mov")
+                .WithColumn("MovieId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Title").AsString(200).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable()
+                .WithColumn("Storyline").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("Year").AsInt32().Nullable()
+                .WithColumn("ReleaseDate").AsDateTime().Nullable()
+                .WithColumn("Runtime").AsInt32().Nullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
